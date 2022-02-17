357a746ff770c78593ee6d566b61494ddd51c8f1
Monica <correo@gmail.com>
2022-02-17 13:42:52 +0100

    java

---
+++ practica9.java
@@ -0,0 +1,10 @@
+public class practica9{
+    public static void main(String args[]){
+        int i = 20;
+        
+        if (i < 15)
+            System.out.println(i + " es menor que 15");
+        else
+            System.out.println(i + " es mayor que 15");
+    }
+} 
