58235c556362258d0d4022c0c6138dfd22577443
Monicamarm <99404491+Monicamarm@users.noreply.github.com>
2022-02-17 13:56:25 +0100

    Update practica9.java

--- practica9.java
+++ practica9.java
@@ -2,9 +2,6 @@ public class practica9{
     public static void main(String args[]){
         int i = 20;
         
-        if (i < 15)
-            System.out.println(i + " es menor que 15");
-        else
-            System.out.println(i + " es mayor que 15");
+      System.out.println(i +" es " + ((i < 15) ? "mayor" : "menor") + " que 15");
     }
 } 
